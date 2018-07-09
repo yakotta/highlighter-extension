9a89ed62e3ab0c3ca211615db5fcb9cf3c62ea83
Jenna <pinkhamjenna@gmail.com>
2018-07-09 01:19:59 -0700

    the selected text now also changes color while selected

--- injection_script.js
+++ injection_script.js
@@ -1,8 +1,17 @@
 // Adds .yakottaHighlighter to <body> tag if not already present
-var bodyElement = document.getElementsByTagName("body")[0];
+var bodyElement = document.getElementsByTagName('body')[0];
 if(!bodyElement.classList.contains('yakottaHighlighter')){
     bodyElement.classList.add('yakottaHighlighter');
 }
+
+// Adds the color class to the body and removes old color classes
+var classList = bodyElement.classList.toString().split(" ");
+for (i = 0; i < classList.length; i++){
+    if(classList[i].includes("yaHiLi")){
+        bodyElement.classList.remove(classList[i]);
+    }
+}
+bodyElement.classList.add(color);
 
 // Determines the selected text
 document.onmouseup = function() {
@@ -12,12 +21,12 @@ document.onmouseup = function() {
 
 // Finds the text selected in the page, spans it, and gives it a class
 function getSelectedText(inputColor) {
-    var span = document.createElement("span");
+    var span = document.createElement('span');
     span.setAttribute('class', inputColor);
 
-    if (document.getSelection) {
+    if(document.getSelection) {
         var selection = document.getSelection();
-        if (selection.rangeCount) {
+        if(selection.rangeCount) {
             var range = selection.getRangeAt(0).cloneRange();
             range.surroundContents(span);
             selection.removeAllRanges();
