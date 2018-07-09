4ee2bf51992887fd15525a321308b8c513467fdf
Jenna <pinkhamjenna@gmail.com>
2018-07-08 18:09:15 -0700

    code cleanup

--- content_script.js
+++ content_script.js
@@ -1,5 +1,6 @@
 // Adds an event listener to the extension's popup
 document.getElementById("colorSelectButton").addEventListener("click", function(){
+    // Determines which color's radio button was clicked
     var color;
     var radios = document.getElementById('popup').elements['highlight'];
     for (i=0; i < radios.length; i++){
@@ -9,13 +10,15 @@ document.getElementById("colorSelectButton").addEventListener("click", function(
         }
     }
 
+    // Puts the color name in quotes so it can be used in chrome.tabs.executeScript
     color = '"' + color + '"'
 
+    // Sends the color information to inject.js to create hilighting behavior
     chrome.tabs.executeScript({
         code: 'var color = ' + color
     }, function(){
         chrome.tabs.executeScript({
-            file: 'inject.js'
+            file: 'injection_script.js'
         })
     });
 });
